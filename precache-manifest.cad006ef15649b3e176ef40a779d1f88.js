d666ac672cb035afee12bb9c0df33f3ec1f4316a
france1988 <france1988@naver.com>
2019-12-17 12:53:18 +0900

    Updates

---
+++ precache-manifest.cad006ef15649b3e176ef40a779d1f88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de82e2dd802bb7248724c4e6a7de3b3d",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "097359f793b5ddf1c700",
+    "url": "/movie_app/static/css/main.21bc3d3e.chunk.css"
+  },
+  {
+    "revision": "6f0599dff9eecd2a0e73",
+    "url": "/movie_app/static/js/2.43e6c5f9.chunk.js"
+  },
+  {
+    "revision": "097359f793b5ddf1c700",
+    "url": "/movie_app/static/js/main.83983cb3.chunk.js"
+  },
+  {
+    "revision": "7259bafd2195d7f70cbd",
+    "url": "/movie_app/static/js/runtime-main.c454ae6d.js"
+  }
+]);
